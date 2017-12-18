9ec0dfb72fae9394c29ed3199d3f3a75f550fbdb
Tim Joosten <Tjoosten3@gmail.com>
2017-12-18 20:56:07 +0100

    Implement front-page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,89 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+    <div class="row">
+        <div class="col-md-9">
+            <h3>Laat Belgie nuclaire wapens verbieden.</h3>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!</p>
+            <p>Ad dolore dignissimos asperiores dicta facere optio quod commodi nam tempore recusandae. Rerum sed nulla eum vero expedita ex delectus voluptates rem at neque quos facere sequi unde optio aliquam!</p>
+            <p>Tenetur quod quidem in voluptatem corporis dolorum dicta sit pariatur porro quaerat autem ipsam odit quam beatae tempora quibusdam illum! Modi velit odio nam nulla unde amet odit pariatur at!</p>
+            <p>Consequatur rerum amet fuga expedita sunt et tempora saepe? Iusto nihil explicabo perferendis quos provident delectus ducimus necessitatibus reiciendis optio tempora unde earum doloremque commodi laudantium ad nulla vel odio?</p>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+            <hr>
 
-            .full-height {
-                height: 100vh;
-            }
+            <form method="POST" class="form-horizontal" action="">
+                {{ csrf_field() }} {{-- CSRF token protection --}}
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+                <div class="form-group">
+                    <label class="control-label col-md-3"> Uw naam: <span class="text-danger">*</span></label>
 
-            .position-ref {
-                position: relative;
-            }
+                    <div class="col-md-4">
+                        <input type="text" class="form-control" placeholder="Uw voornaam">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+                    </div>
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                    <div class="col-md-5">
+                        <input type="text" class="form-control" placeholder="Uw achternaam">
+                    </div>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="form-group">
+                    <label class="control-label col-md-3">Geboortedatum: <span class="text-danger">*</span> </label>
+
+                    <div class="col-md-9">
+                    <input type="date" class="form-control">
                 </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3"> Uw adres: <span class="text-danger">*</span></label>
+
+            <div class="col-md-6 @error('address' , 'has-error')">
+                <input type="text" class="form-control" placeholder="Uw straatcnaam">
+            </div>
+
+            <div class="col-md-3 @error('house_number', 'has-error')">
+                <input type="text" class="form-control" placeholder="Uw Huisnummer">
             </div>
         </div>
-    </body>
-</html>
+
+        <div class="form-group">
+            <div class="col-md-offset-3 col-md-3 @error('postal_code', 'has-error')">
+                <input type="text" class="form-control" placeholder="Stadsnaam">
+            </div>
+
+            <div class="col-md-6">
+                <input type="text" class="form-control" placeholder="Stadsnaam">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-offset-3 col-md-9">
+                <button type="submit" class="btn btn-sm btn-success">
+                    <i class="fa fa-check"></i> Tekenen
+                </button>
+
+                <button type="reset" class="btn btn-sm btn-link">
+                    <i class="fa fa-undo"></i> Annuleren
+                </button>
+            </div>
+        </div>
+        </form>
+
+        </div>
+
+        <div class="col-md-3">
+            <div class="p-30">
+                <a class="btn btn-block btn-social btn-facebook">
+                    <span class="fa fa-facebook"></span>
+                    Deel op facebook
+                </a>
+                <a class="btn btn-block btn-social btn-twitter">
+                    <span class="fa fa-twitter"></span>
+                    Deel op Twitter
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
