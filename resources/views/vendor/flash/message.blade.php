4d15a1ab62f0ff6c1257dc271521c2306c53d29a
Tim Joosten <Tjoosten3@gmail.com>
2017-12-19 01:30:39 +0100

    Reformat flash sessie weergave

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -1,22 +1,10 @@
 @foreach (session('flash_notification', collect())->toArray() as $message)
     @if ($message['overlay'])
-        @include('flash::modal', [
-            'modalClass' => 'flash-modal',
-            'title'      => $message['title'],
-            'body'       => $message['message']
-        ])
+        @include('flash::modal', ['modalClass' => 'flash-modal', 'title' => $message['title'], 'body' => $message['message']])
     @else
-        <div class="alert
-                    alert-{{ $message['level'] }}
-                    {{ $message['important'] ? 'alert-important' : '' }}"
-                    role="alert"
-        >
+        <div class="alert alert-{{ $message['level'] }} {{ $message['important'] ? 'alert-important' : '' }}" role="alert">
             @if ($message['important'])
-                <button type="button"
-                        class="close"
-                        data-dismiss="alert"
-                        aria-hidden="true"
-                >&times;</button>
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             @endif
 
             {{ $message['message'] }}
