97d11c139b1f6a2ecf826d4c7a5c25da0715b81d
Tjoosten <Tjoosten3@gmail.com>
2017-12-19 02:43:41 +0100

    Vervolledig authencatie translaties

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,13 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+                <div class="panel-heading">
+                    Reset wachtwoord
+                
+                    <a href="{{ route('frontend.index') }}" class="btn btn-xs btn-link pull-right">
+                        <i class="fa fa-undo"></i> Ga terug
+                    </a>
+                </div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -18,7 +24,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail Adres</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
@@ -34,7 +40,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
+                                    <i class="fa fa-check"></i> Zend wachtwoord reset link.
                                 </button>
                             </div>
                         </div>
