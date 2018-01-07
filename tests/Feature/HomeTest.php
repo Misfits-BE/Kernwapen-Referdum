2ca9882c25ac4ecb59791ec06eaf770609e0f979
Tim Joosten <Tjoosten3@gmail.com>
2018-01-07 03:27:14 +0100

    Backend home test

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -5,10 +5,12 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class HomeTest extends TestCase
 {
-    use DatabaseTransactions, DatabaseMigrations;
+    use RefreshDatabase;
     
     /**
      * @test
@@ -17,5 +19,19 @@ class HomeTest extends TestCase
     public function frontendIndex()
     {
         $this->get(route('frontend.index'))->assertStatus(200);
+    }
+
+    /**
+     * @test 
+     * @testdox Test of de backend index page successvol kan renderen 
+     */
+    public function backendIndex() 
+    {
+        $user = factory(User::class)->create(); 
+        
+        $this->actingAs($user)
+            ->assertAuthenticatedAs($user)
+            ->get(route('backend.index'))
+            ->assertStatus(200);
     }
 }
