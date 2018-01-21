0032b26346259b1621cd29b3aac97504d709b73f
Tim Joosten <Tjoosten3@gmail.com>
2018-01-21 21:29:22 +0100

    Ref #54 | Register empty user policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -12,7 +12,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model' => 'App\Policies\UserPolicy',
     ];
 
     /**
@@ -20,10 +20,8 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->registerPolicies();
-
-        //
     }
 }
