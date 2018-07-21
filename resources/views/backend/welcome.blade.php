cc10b7c50c5c95f4c7902ac07390cc8b0bb6d7bc
Tim Joosten <Tjoosten3@gmail.com>
2018-07-21 14:37:32 +0200

    update

--- resources/views/backend/welcome.blade.php
+++ resources/views/backend/welcome.blade.php
@@ -2,21 +2,36 @@
 
 @section('content')
     <div class="container">
-        <div class="row"> {{-- Panel cards --}}
-            <div class="col-md-3">
-                <div class="panel panel-default"> {{-- Signature card --}}
+        <div class="row">
+            <div class="col-md-3"> {{-- Signature counter --}}
+                <div class="panel panel-default">
                     <div class="panel-body p-3">
                         <div class="d-flex align-items-center">
                             <span class="stamp tw-shadow stamp-md bg-red mr-3">
                                 <i class="fa fa-pencil" aria-hidden="true"></i>
                             </span>
-                        <div>
-                      
-                        <h4 class="m-0"><a class="text-muted" href="">1,352 <small>Handtekeningen</small></a></h4>
-                        <small class="text-muted">163 handtekeningen vandaag</small>
+
+                            <div>
+                                <h4 class="m-0"><a class="text-muted" href="">1,352 <small>Handtekeningen</small></a></h4>
+                                <small class="text-muted">163 handtekeningen vandaag</small>
+                            </div>
+                        </div>
                     </div>
-                </div> {{-- /// Signature card --}}
-            </div>
-        </div> {{-- /// Panel cards --}}
-    </div>
+                </div>  
+            </div> {{-- /// Signatures counter --}}
+
+            <div class="col-md-3"> {{-- Kernwapen vrije gemeentes --}}
+            </div> {{-- /// Kernwapen vrije gemeentes --}}
+
+            <div class="col-md-12"> {{-- Recent signatures table --}}
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="fa fa-fw fa-pencil" aria-hidden="true"></i> Recente handtekeningen
+                    </div>
+                    <div class="panel-body">
+                    </div>
+                </div>
+            </div> {{-- /// Recent signatures table --}}
+        </div>
+    </div>   
 @endsection
